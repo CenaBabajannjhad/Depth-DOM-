7c19ee60f1a1f74eefac88dd588d3c1939812bfe
Cena <cenababajannjhad@gmail.com>
2024-10-22 20:02:29 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Get the reference to the container div
-var container = document.getElementById("container");
+const container = document.getElementById("container");
 
 // Create the initial div
-var firstDiv = document.createElement("div");
+const firstDiv = document.createElement("div");
 firstDiv.textContent = "Item 1";
 container.appendChild(firstDiv);
 
 // Loop 19 times (since we already have one div)
 //2293
-for (var i = 2; i <= 3; i++) {   
+for (let i = 2; i <= 3; i++) {   
     // Create a new div
-    var newDiv = document.createElement("div");
+    const newDiv = document.createElement("div");
     // Set its content
     newDiv.textContent = "Item " + i;
     // Append it inside the previous div
